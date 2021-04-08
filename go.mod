090a938a305418e60ccc006bf8a3f538a8d04b4b
Grant Griffiths <ggriffiths@purestorage.com>
2021-04-07 17:09:47 -0700

    Add openstorage vendor
    Signed-off-by: Grant Griffiths <ggriffiths@purestorage.com>

--- go.mod
+++ go.mod
@@ -1,25 +1,67 @@
-module github.com/kubernetes-csi/csi-test/v4
+module github.com/libopenstorage/csi-test
 
 go 1.15
 
 require (
+	github.com/cloudfoundry/gosigar v1.1.0 // indirect
 	github.com/container-storage-interface/spec v1.3.0
-	github.com/go-logr/logr v0.3.0 // indirect
+	github.com/gobuffalo/packr v1.30.1 // indirect
 	github.com/golang/mock v1.4.4
 	github.com/golang/protobuf v1.4.3
-	github.com/google/uuid v1.1.2
-	github.com/nxadm/tail v1.4.5 // indirect
-	github.com/onsi/ginkgo v1.14.2
-	github.com/onsi/gomega v1.10.4
+	github.com/google/uuid v1.2.0
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2 // indirect
+	github.com/kubernetes-csi/csi-test v2.2.0+incompatible
+	github.com/kubernetes-csi/csi-test/v4 v4.1.0
+	github.com/libopenstorage/openstorage v8.0.1-0.20190926212733-daaed777713e+incompatible
+	github.com/libopenstorage/systemutils v0.0.0-20180202003325-dc0e35340804 // indirect
+	github.com/onsi/ginkgo v1.15.0
+	github.com/onsi/gomega v1.10.5
+	github.com/pborman/uuid v1.2.1 // indirect
+	github.com/portworx/kvdb v0.0.0-20210316224406-4c29d8da7240
+	github.com/prometheus/client_golang v1.10.0 // indirect
 	github.com/robertkrimen/otto v0.0.0-20200922221731-ef014fd054ac
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/sirupsen/logrus v1.7.0
-	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11
-	golang.org/x/sys v0.0.0-20201207223542-d4d67f95c62d // indirect
-	golang.org/x/text v0.3.4 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/genproto v0.0.0-20201209185603-f92720507ed4 // indirect
+	github.com/stretchr/testify v1.7.0
+	github.com/vbatts/tar-split v0.11.1 // indirect
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
 	google.golang.org/grpc v1.34.0
-	gopkg.in/sourcemap.v1 v1.0.5 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 	k8s.io/klog/v2 v2.4.0
+)
+
+replace (
+	github.com/docker/docker => github.com/moby/moby v20.10.3-0.20210324213045-797b974cb90e+incompatible
+	github.com/hashicorp/consul => github.com/hashicorp/consul v1.5.1
+	github.com/kubernetes-incubator/external-storage => github.com/libopenstorage/external-storage v0.20.4-openstorage-rc3
+	github.com/libopenstorage/openstorage => github.com/libopenstorage/openstorage v0.0.0-k8s120-rc1
+	github.com/libopenstorage/openstorage v8.0.1-0.20190926212733-daaed777713e+incompatible => github.com/libopenstorage/openstorage v0.0.0-k8s120-rc1
+	github.com/satori/go.uuid => github.com/satori/go.uuid v0.0.0-20160324112244-f9ab0dce87d8
+	google.golang.org/grpc => google.golang.org/grpc v1.29.1
+
+	k8s.io/api => k8s.io/api v0.20.4
+	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.20.4
+	k8s.io/apimachinery => k8s.io/apimachinery v0.20.4
+	k8s.io/apiserver => k8s.io/apiserver v0.20.4
+	k8s.io/cli-runtime => k8s.io/cli-runtime v0.20.4
+	k8s.io/client-go => k8s.io/client-go v0.20.4
+	k8s.io/cloud-provider => k8s.io/cloud-provider v0.20.4
+	k8s.io/cluster-bootstrap => k8s.io/cluster-bootstrap v0.20.4
+	k8s.io/code-generator => k8s.io/code-generator v0.20.4
+	k8s.io/component-base => k8s.io/component-base v0.20.4
+	k8s.io/component-helpers => k8s.io/component-helpers v0.20.4
+	k8s.io/controller-manager => k8s.io/controller-manager v0.20.4
+	k8s.io/cri-api => k8s.io/cri-api v0.20.4
+	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.20.4
+	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.20.4
+	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.20.4
+	k8s.io/kube-proxy => k8s.io/kube-proxy v0.20.4
+	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.20.4
+	k8s.io/kubectl => k8s.io/kubectl v0.20.4
+	k8s.io/kubelet => k8s.io/kubelet v0.20.4
+	k8s.io/kubernetes => k8s.io/kubernetes v1.20.4
+	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.20.4
+	k8s.io/metrics => k8s.io/metrics v0.20.4
+	k8s.io/mount-utils => k8s.io/mount-utils v0.20.4
+	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.20.4
 )
---
+++ vendor/github.com/libopenstorage/openstorage/csi/node.go
@@ -0,0 +1,303 @@
+/*
+Package csi is CSI driver interface for OSD
+Copyright 2017 Portworx
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package csi
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/libopenstorage/openstorage/api"
+	"github.com/portworx/kvdb"
+
+	csi "github.com/container-storage-interface/spec/lib/go/csi"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *OsdCsiServer) NodeGetInfo(
+	ctx context.Context,
+	req *csi.NodeGetInfoRequest,
+) (*csi.NodeGetInfoResponse, error) {
+
+	clus, err := s.cluster.Enumerate()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Unable to Enumerate cluster: %s", err)
+	}
+
+	result := &csi.NodeGetInfoResponse{
+		NodeId: clus.NodeId,
+	}
+
+	return result, nil
+}
+
+// NodePublishVolume is a CSI API call which mounts the volume on the specified
+// target path on the node.
+//
+// TODO: Support READ ONLY Mounts
+//
+func (s *OsdCsiServer) NodePublishVolume(
+	ctx context.Context,
+	req *csi.NodePublishVolumeRequest,
+) (*csi.NodePublishVolumeResponse, error) {
+
+	logrus.Debugf("NodePublishVolume req[%#v]", req)
+
+	// Check arguments
+	if len(req.GetVolumeId()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume id must be provided")
+	}
+	if len(req.GetTargetPath()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Target path must be provided")
+	}
+	if req.GetVolumeCapability() == nil || req.GetVolumeCapability().GetAccessMode() == nil {
+		return nil, status.Error(codes.InvalidArgument, "Volume access mode must be provided")
+	}
+	if req.GetVolumeCapability().GetBlock() != nil {
+		return nil, status.Errorf(codes.Unimplemented, "CSI raw block is not supported")
+	}
+
+	// Ensure target location is created correctly
+	if err := ensureMountPathCreated(req.GetTargetPath()); err != nil {
+		return nil, status.Errorf(
+			codes.Aborted,
+			"Failed to use target location %s: %s",
+			req.GetTargetPath(),
+			err.Error())
+	}
+
+	// Get grpc connection
+	conn, err := s.getConn()
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Unable to connect to SDK server: %v", err)
+	}
+
+	// Get secret if any was passed
+	ctx = s.setupContextWithToken(ctx, req.GetSecrets())
+
+	// Check if block device
+	driverType := s.driver.Type()
+	if driverType != api.DriverType_DRIVER_TYPE_BLOCK &&
+		req.GetVolumeCapability().GetBlock() != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Trying to attach as block a non block device")
+	}
+
+	// Gather volume attributes
+	spec, locator, _, err := s.specHandler.SpecFromOpts(req.GetVolumeContext())
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			"Invalid volume attributes: %#v",
+			req.GetVolumeContext())
+	}
+
+	// Get volume encryption info from req.Secrets
+	driverOpts := s.addEncryptionInfoToLabels(make(map[string]string), req.GetSecrets())
+
+	// prepare for mount/attaching
+	mounts := api.NewOpenStorageMountAttachClient(conn)
+	opts := &api.SdkVolumeAttachOptions{
+		SecretName: spec.GetPassphrase(),
+	}
+
+	// can use either spec.Ephemeral or VolumeContext label
+	volumeId := req.GetVolumeId()
+	if req.GetVolumeContext()["csi.storage.k8s.io/ephemeral"] == "true" || spec.Ephemeral {
+		spec.Ephemeral = true
+		volumes := api.NewOpenStorageVolumeClient(conn)
+		resp, err := volumes.Create(ctx, &api.SdkVolumeCreateRequest{
+			Name:   req.GetVolumeId(),
+			Spec:   spec,
+			Labels: locator.GetVolumeLabels(),
+		})
+		if err != nil {
+			return nil, err
+		}
+		volumeId = resp.VolumeId
+	}
+
+	if driverType == api.DriverType_DRIVER_TYPE_BLOCK {
+		if _, err = mounts.Attach(ctx, &api.SdkVolumeAttachRequest{
+			VolumeId:      volumeId,
+			Options:       opts,
+			DriverOptions: driverOpts,
+		}); err != nil {
+			if spec.Ephemeral {
+				logrus.Errorf("Failed to attach ephemeral volume %s: %v", volumeId, err.Error())
+				s.cleanupEphemeral(ctx, conn, volumeId, false)
+			}
+			return nil, err
+		}
+	}
+
+	// Mount volume onto the path
+	if _, err := mounts.Mount(ctx, &api.SdkVolumeMountRequest{
+		VolumeId:  volumeId,
+		MountPath: req.GetTargetPath(),
+		Options:   opts,
+	}); err != nil {
+		if spec.Ephemeral {
+			logrus.Errorf("Failed to mount ephemeral volume %s: %v", volumeId, err.Error())
+			s.cleanupEphemeral(ctx, conn, volumeId, true)
+		}
+		return nil, err
+	}
+
+	logrus.Infof("Volume %s mounted on %s",
+		volumeId,
+		req.GetTargetPath())
+
+	return &csi.NodePublishVolumeResponse{}, nil
+}
+
+// NodeUnpublishVolume is a CSI API call which unmounts the volume.
+func (s *OsdCsiServer) NodeUnpublishVolume(
+	ctx context.Context,
+	req *csi.NodeUnpublishVolumeRequest,
+) (*csi.NodeUnpublishVolumeResponse, error) {
+
+	logrus.Debugf("NodeUnPublishVolume req[%#v]", req)
+
+	// Check arguments
+	if len(req.GetVolumeId()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Volume id must be provided")
+	}
+	if len(req.GetTargetPath()) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Target path must be provided")
+	}
+
+	// Get volume information
+	vols, err := s.driver.Inspect([]string{req.GetVolumeId()})
+	if err != nil || len(vols) < 1 {
+		if err == kvdb.ErrNotFound {
+			logrus.Infof("Volume %s was deleted or cannot be found: %s", req.GetVolumeId(), err.Error())
+			return &csi.NodeUnpublishVolumeResponse{}, nil
+		} else if err != nil {
+			return nil, status.Errorf(codes.NotFound, "Volume id %s not found: %s",
+				req.GetVolumeId(),
+				err.Error())
+		} else {
+			logrus.Infof("Volume %s was deleted or cannot be found", req.GetVolumeId())
+			return &csi.NodeUnpublishVolumeResponse{}, nil
+		}
+	}
+
+	// Mount volume onto the path
+	if err = s.driver.Unmount(req.GetVolumeId(), req.GetTargetPath(), nil); err != nil {
+		logrus.Infof("Unable to unmount volume %s onto %s: %s",
+			req.GetVolumeId(),
+			req.GetTargetPath(),
+			err.Error())
+	}
+
+	if s.driver.Type() == api.DriverType_DRIVER_TYPE_BLOCK {
+		if err = s.driver.Detach(req.GetVolumeId(), nil); err != nil {
+			return nil, status.Errorf(
+				codes.Internal,
+				"Unable to detach volume: %s",
+				err.Error())
+		}
+	}
+
+	// Attempt to remove volume path
+	// Kubernetes handles this after NodeUnpublishVolume finishes, but this allows for cross-CO compatibility
+	if err := os.Remove(req.GetTargetPath()); err != nil && !os.IsNotExist(err) {
+		logrus.Warnf("Failed to delete mount path %s: %s", req.GetTargetPath(), err.Error())
+	}
+
+	// Return error to Kubelet if mount path still exists to force a retry
+	if _, err := os.Stat(req.GetTargetPath()); !os.IsNotExist(err) {
+		return nil, status.Errorf(
+			codes.Internal,
+			"Mount path still exists: %s",
+			req.GetTargetPath())
+	}
+
+	logrus.Infof("Volume %s unmounted from path %s", req.GetVolumeId(), req.GetTargetPath())
+
+	return &csi.NodeUnpublishVolumeResponse{}, nil
+}
+
+// NodeGetCapabilities is a CSI API function which seems to be setup for
+// future patches
+func (s *OsdCsiServer) NodeGetCapabilities(
+	ctx context.Context,
+	req *csi.NodeGetCapabilitiesRequest,
+) (*csi.NodeGetCapabilitiesResponse, error) {
+
+	logrus.Debugf("NodeGetCapabilities req[%#v]", req)
+
+	return &csi.NodeGetCapabilitiesResponse{
+		Capabilities: []*csi.NodeServiceCapability{
+			{
+				Type: &csi.NodeServiceCapability_Rpc{
+					Rpc: &csi.NodeServiceCapability_RPC{
+						Type: csi.NodeServiceCapability_RPC_UNKNOWN,
+					},
+				},
+			},
+		},
+	}, nil
+}
+
+// cleanupEphemeral detaches and deletes an ephemeral volume if either attach or mount fails
+func (s *OsdCsiServer) cleanupEphemeral(ctx context.Context, conn *grpc.ClientConn, volumeId string, detach bool) {
+	if detach {
+		mounts := api.NewOpenStorageMountAttachClient(conn)
+		if _, err := mounts.Detach(ctx, &api.SdkVolumeDetachRequest{
+			VolumeId: volumeId,
+		}); err != nil {
+			logrus.Errorf("Failed to detach ephemeral volume %s during cleanup: %v", volumeId, err.Error())
+			return
+		}
+	}
+	volumes := api.NewOpenStorageVolumeClient(conn)
+	if _, err := volumes.Delete(ctx, &api.SdkVolumeDeleteRequest{
+		VolumeId: volumeId,
+	}); err != nil {
+		logrus.Errorf("Failed to delete ephemeral volume %s during cleanup: %v", volumeId, err.Error())
+	}
+}
+
+func ensureMountPathCreated(targetPath string) error {
+	fileInfo, err := os.Lstat(targetPath)
+	if err != nil && os.IsNotExist(err) {
+		err = os.MkdirAll(targetPath, 0750)
+		if err != nil {
+			return fmt.Errorf(
+				"Failed to create target path %s: %s",
+				targetPath,
+				err.Error())
+		}
+	} else if err != nil {
+		return fmt.Errorf(
+			"Unknown error while verifying target location %s: %s",
+			targetPath,
+			err.Error())
+	} else {
+		if !fileInfo.IsDir() {
+			return fmt.Errorf("Target location %s is not a directory", targetPath)
+		}
+	}
+
+	return nil
+}
